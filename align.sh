412a349ae4084468f970d42d9f4ca436e6e5988f
Takefumi Yoshikawa (beyond) <takefumi.biology@gmail.com>
2018-01-08 17:10:55 +0900

    change dir str

--- align.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/zsh
-
-cd $WRDATA/protein
-
-for seqfile in `find ./og/ -maxdepth 1 -name "*.fas" -type f`; do
-    mafft-linsi --anysymbol $seqfile > ${seqfile/og/alignment}
-done
-
-cd $WRPROJ
